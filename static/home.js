8e7e64a53f71147ee11e046301f11bc458e481ee
Nannan <nanna7077@gmail.com>
2021-08-20 05:53:06 +0000

    Add files via upload

---
+++ static/home.js
@@ -0,0 +1,45 @@
+function shuffle(array) {
+    var currentIndex = array.length,
+        randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+
+    return array;
+}
+
+function workwithdata(data) {
+    var final_array = [];
+    final_array = shuffle(data['wallpaper']);
+    var container_ = document.getElementById("randomContainer");
+    container_.innerHTML = '';
+    for (i in final_array) {
+        tagstr = '';
+        arr = final_array[i]['tags'];
+        console.log(arr);
+        for (j in arr) {
+            tagstr += arr[j];
+            tagstr += ", ";
+        }
+        tagstr = tagstr.slice(0, -2);
+        container_.innerHTML +=
+            "<div class=\"glassyContainer\"> <img class = \"imgDisp\" loading=\"lazy\" src = \"" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>" + tagstr + "<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") + "</b></div></div>";
+    }
+
+};
+
+$.ajax({
+    url: "static/tags.json",
+    success: function(data) {
+        workwithdata(data);
+    },
+    error: function(data) {
+        console.log("Couldn't fetch json");
+    },
+    dataType: "json",
+    timeout: 3000
+});
