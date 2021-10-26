de1b99e504e254cb05c0e994af92aba957fb5539
Nannan <nanna7077@gmail.com>
2021-10-26 21:49:17 +0530

    Changes: Allow download from webpage; make blocks responsive;

--- static/home.js
+++ static/home.js
@@ -14,6 +14,28 @@ function shuffle(array) {
     return array;
 }
 
+function openfullimage(id) {
+    tagstr = '';
+    id = parseInt(id);
+    for (i in theWallpaperDict) {
+        if (theWallpaperDict[i]['path'].replace(/\D/g, "") == id) {
+            elem = theWallpaperDict[i];
+        }
+    }
+    arr = elem['tags'];
+    for (j in arr) {
+        tagstr += arr[j];
+        tagstr += ", ";
+    }
+    tagstr = tagstr.slice(0, -2);
+    document.getElementById("fulldisp_img").src = "https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/" + elem['path'];
+    document.getElementById("fullimagedisplay_name").innerHTML = elem['name'];
+    document.getElementById("fullimagedisplay_desc").innerHTML = elem['description'];
+    document.getElementById("fullimagedisplay_tags").innerHTML = tagstr;
+    document.getElementById("fullimagedisplay_link_download_primary").href = "https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/" + elem['path'];
+    document.getElementById("fullimagedisplay_link_download_secondary").href = "https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/" + elem['path'];
+    document.getElementById("fullimg").style.display = 'block';
+}
 
 function onlyTags(tag) {
     final_array = shuffle(theWallpaperDict);
@@ -29,7 +51,7 @@ function onlyTags(tag) {
             }
             tagstr = tagstr.slice(0, -2);
             container_.innerHTML +=
-                "<div class=\"glassyContainer\"> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") + "</b></div></div>";
+                "<div class=\"glassyContainer\" onclick=openfullimage(" + final_array[i]['path'].replace(/\D/g, "") + ")> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "</b></div></div>"; //<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") + 
         }
     }
 }
@@ -48,7 +70,7 @@ function workwithdata(data) {
         }
         tagstr = tagstr.slice(0, -2);
         container_.innerHTML +=
-            "<div class=\"glassyContainer\"> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") + "</b></div></div>";
+            "<div class=\"glassyContainer\" onclick=openfullimage(" + final_array[i]['path'].replace(/\D/g, "") + ")> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "</b></div></div>"; //"<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") +
     }
     var tagContainer = document.getElementById("tagContainer");
     tagContainer.innerHTML = '';
@@ -77,4 +99,11 @@ $.ajax({
     },
     dataType: "json",
     timeout: 3000
+});
+
+window.addEventListener('mouseup', function(event) {
+    var pol = document.getElementById('fullimg');
+    if (event.target != pol && event.target.parentNode != pol) {
+        pol.style.display = 'none';
+    }
 });
